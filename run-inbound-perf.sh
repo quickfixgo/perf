0c623ef41b11ac9a8e37c5ffd3c8ed40906e5f22
Timour Ezeev <timour.ezeev@me.com>
2015-08-18 14:50:22 -0400

    Fixed hanging due to application processes mis-syncronization on signal

--- run-inbound-perf.sh
+++ run-inbound-perf.sh
@@ -3,11 +3,14 @@
 SAMPLE_SIZE=$1
 [ -z $SAMPLE_SIZE ] && SAMPLE_SIZE=1000
 
-./inbound/inbound -cpuprofile=inbound.prof -fixconfig=cfg/inbound.cfg  -samplesize=$SAMPLE_SIZE &
+# ./inbound/inbound -cpuprofile=inbound.prof -fixconfig=cfg/inbound.cfg  -samplesize=$SAMPLE_SIZE &
+./inbound/inbound -fixconfig=cfg/inbound.cfg  -samplesize=$SAMPLE_SIZE &
 RETVAL=$?
 [ $RETVAL -ne 0 ] && exit $RETVAL
 
 INBOUND_PID=$!
+
+trap "kill $INBOUND_PID" 1 2 3 9 15
 
 ./outbound/outbound -fixconfig=cfg/outbound.cfg -samplesize=$SAMPLE_SIZE 
 RETVAL=$?
